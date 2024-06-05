90353f78d7b5df376aab1d6055e7914f071c84bc
Alexandre <albarthelme@gmail.com>
2024-06-05 11:50:50 +0200

    Liste to Dict

--- comm_generation/com.py
+++ comm_generation/com.py
@@ -6,10 +6,11 @@ def GenListeCom():
     Generate a list of communications with random sizes and random time between communications (Poisson law)
 
     Returns:
-        None
+        List[Dict]: A list of dictionaries containing communication information
     
     """
     ComTimeStamp=0
+    all_communication_info = []
     for com_num in range(0, 10): # Generate 10 communications
         com_num += 1
         communication_sizes= np.random.poisson(10, 1)  # Average size of 10 Kbits
@@ -17,9 +18,18 @@ def GenListeCom():
         ComTimeStamp+=sleeptimerandom # Add the sleep time to the timestamp, next com will be spawned after the sleep time + the previous timestamp
         communication_quality = np.random.choice([1, 2, 4, 5, 6, 8], 1)[0] # Random quality between BPSK, QPSK, 16QAM, 32QAM, 64QAM, 256QAM
 
-        print("Com Num:", com_num, "| Size:", communication_sizes,"Kbits"," | Com quality:",communication_quality, "bits/symbol | Spawning Time:", round(ComTimeStamp,2),"s", "| Sleep Time:", round(sleeptimerandom,2),"s")
-        print("--------------------------------------")
-        # time.sleep(sleeptimerandom)
+        #print("Com Num:", com_num, "| Size:", communication_sizes,"Kbits"," | Com quality:",communication_quality, "bits/symbol | Spawning Time:", round(ComTimeStamp,2),"s", "| Sleep Time:", round(sleeptimerandom,2),"s")
+        #print("--------------------------------------")
 
-if __name__ == "__main__":
-    GenListeCom()
+
+        com_info = {
+            "Com_Num": com_num,
+            "Size_Kbits": communication_sizes,
+            "Quality_bits_per_symbol": communication_quality,
+            "Spawning_Time_s": round(ComTimeStamp, 2),
+            "Sleep_Time_s": round(sleeptimerandom, 2)
+        }
+        
+        all_communication_info.append(com_info)
+    
+    return all_communication_info
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello, World!")
+from comm_generation.com import GenListeCom
+
+if __name__ == "__main__":
+    print("Generating communications")
+    ListeOfAllCom = GenListeCom()
+
+    for com in ListeOfAllCom:
+        print("Com Num:", com["Com_Num"], "| Size:", com["Size_Kbits"],"Kbits"," | Com quality:",com["Quality_bits_per_symbol"], "bits/symbol | Spawning Time:", com["Spawning_Time_s"],"s", "| Sleep Time:", com["Sleep_Time_s"],"s")
