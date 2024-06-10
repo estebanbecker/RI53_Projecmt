0eb1313d587534697d73e74cd6ab32d0d612d82f
Tom <tom.palleau@gmail.com>
2024-06-10 16:39:36 +0200

    round robin function connected to GenListCom and table

--- main.py
+++ main.py
@@ -7,20 +7,14 @@ from Round_robin import roundRobin
 if __name__ == "__main__":
     setting_values = settings.settings()
 
-    ListeOfAllCom = GenListeCom(settings_values=setting_values)
+    list_com = GenListeCom(settings_values=setting_values)
 
-    allocation_grid = roundRobin.round_robin(ListeOfAllCom, int(setting_values[0]))
+    allocation_grid = roundRobin.round_robin(list_com, int(setting_values[0]))
 
-    for com in ListeOfAllCom:
+    for com in list_com:
         print("Com Num:", com["Com_Num"], "| Size:", com["Size_Kbits"],"Kbits"," | Com quality:",com["Quality_bits_per_symbol"], "bits/symbol | Spawning Time:", com["Spawning_Time_s"],"s", "| Sleep Time:", com["Sleep_Time_s"],"s")
 
+    resource_grid = roundRobin.round_robin(list_com, len(setting_values))
 
-    # generate values with settings values
-    #
-    # ...
-    #
-
-    # temporary values for test
-
-    table.plot_resource_grid(allocation_grid.get_full_allocation_grid_np)
-    print("Generating communications")
+    table.plot_resource_grid(resource_grid.get_full_allocation_grid_np())
+    # print("Generating communications")
