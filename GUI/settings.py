2e8b7e917d3b48adc06b5204e7195a21b9c30e13
Tom <tom.palleau@gmail.com>
2024-06-12 10:52:22 +0200

    settings bandwidth is a selector and output has named keys instead of numbered keys

--- GUI/settings.py
+++ GUI/settings.py
@@ -2,15 +2,16 @@ import PySimpleGUI as sg
 
 def settings() -> dict:
     setting_values = {}
+
     sg.theme("DarkAmber")
 
-    param = [[sg.Text("frequence"), sg.Input(default_text=10, size=(4,1))],
-            [sg.Text("communication"), sg.Input(size=(4,1))],
-            [sg.Text("moyenne d'arrivée"), sg.Input(size=(4,1))],
-            [sg.Text("Ecart-type"), sg.Input(size=(4,1))],
-            [sg.Text("tailles des communication"), sg.Input(size=(4,1))],
-            [sg.Text("taille moyenne"), sg.Input(size=(4,1))],
-            [sg.Text("ecart-Type"), sg.Input(size=(4,1))],
+    param = [[sg.Text("Bandwidth in MHz: "), sg.Combo([1.4, 3, 5, 10, 15, 20],default_value=1.4, size=(4,1), key="bandwidth")], 
+            [sg.Text("Nombre de communications"), sg.Input(size=(4,1), key="com", default_text=2)], 
+            [sg.Text("Temps Max entre 2 com (def : 1s)"), sg.Input(size=(4,1), key="interval", default_text=2)], 
+            #[sg.Text("Ecart-type"), sg.Input(size=(4,1))],
+            #[sg.Text("tailles des communication"), sg.Input(size=(4,1))],
+            [sg.Text("taille moyenne des comm (def : 10kbts)"), sg.Input(size=(4,1), key="av_size", default_text=2)],
+            #[sg.Text("ecart-Type"), sg.Input(size=(4,1))],
             [sg.Ok(key="-Generate-"), sg.Cancel(key="-CANCEL-")]]
 
     layout = [[sg.Column(param)]]
@@ -21,13 +22,20 @@ def settings() -> dict:
         event, values = window.read()
         
         if event == sg.WIN_CLOSED or event == "-CANCEL-":
-            break
+            window.close()
+            exit()
         
         if event == "-Generate-":
             setting_values = values
+            
+            bandwidth_rb = {1.4: 6, 3: 15, 5: 25, 10: 50, 15: 75, 20: 100}
+            setting_values["RB"] = bandwidth_rb[setting_values["bandwidth"]]
             break
             
 
     window.close()
     
     return setting_values
+
+if __name__ == "__main__":
+    print(settings())
